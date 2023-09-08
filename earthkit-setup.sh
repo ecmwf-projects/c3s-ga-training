f3855ed5e098c5db7b188182ed776aa693c1ef0d
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-09-08 08:43:28 +0100

    environment building

--- earthkit-setup.sh
+++ earthkit-setup.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
 
 envname=earthkit
-conda activate $envname 2>/dev/null || conda create -n $envname -c conda-forge python=3.10 -y && conda activate $envname;
+conda create -n $envname -c conda-forge -y python=3.10
+conda env update -n $envname -f environment.yml
+conda activate $envname
 
-pip install earthkit@git+https://github.com/ecmwf/earthkit@feature/import-resilience
+. ./postBuild
 
-# Install EK packages
-# pip install earthkit-data@git+https://github.com/ecmwf/earthkit-data@develop --upgrade
-pip install ecmwflibs
-pip install earthkit-data@git+https://github.com/ecmwf/earthkit-data@develop --upgrade
-# conda install -y -c conda-forge earthkit-data 
-pip install earthkit-maps --upgrade
-# Replace with PyPi when released
-pip install earthkit-climate --upgrade
-
-# Additional dependancies:
-conda install -c conda-forge -y cf-units cartopy rasterio
-
-# Install jupyter
-conda install -y -c conda-forge jupyter
-pip install ipykernel
-python -m ipykernel install --user --name=earthkit
+# set earthkit environment to the default used by ipykernels
+python -m ipykernel install --user --name=$envname
